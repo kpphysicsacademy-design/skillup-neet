126bf13ac1662b2892b6dee3e4a7bfcfbc15477e
kpphysicsacademy-design <kpphysicsacademy@gmail.com>
2026-09-05 22:18:32 +0530

    Migrate Firebase client configuration for NEET practice

---
+++ firebase-web-config.js
@@ -0,0 +1,11 @@
+// SkillUp Firebase Web configuration.
+// Public client configuration; authorization is enforced by Firebase Auth and Firestore Rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDjCCKECq2UyOQhRg7oIW3tIpk86Ab1FJs",
+  authDomain: "skill-up-j6olyb.firebaseapp.com",
+  projectId: "skill-up-j6olyb",
+  storageBucket: "skill-up-j6olyb.firebasestorage.app",
+  messagingSenderId: "112776254896",
+  appId: "1:112776254896:web:7f10fde74fdf1a063a7275",
+  measurementId: "G-LPLJSBNEH4"
+};
